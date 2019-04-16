de5e0b6d814c3f2678b57628737115f3a5d435ae
YAOYELU <luyaoyejeren@gmail.com>
2019-04-16 17:58:20 +1000

    [using pickle in ping successor and transfer file function] but receive part encountered problem, cant figure out yet

--- cdht.py
+++ cdht.py
@@ -4,6 +4,8 @@ import time
 import socket
 import select
 import threading
+import pickle
+MSS = 300
 
 class dhtNode:
     def __init__(self, host, id, fir_successor, sec_successor):
@@ -44,47 +46,55 @@ class ListenToPing(threading.Thread):
         Ack = 0
         buffer = {}
         isdone =False
+        d = b""
         while True:
             data, addr = LTPsock.recvfrom(1024)
             sending_peer = addr[1] - 50000
             if data:
-                # if "Ping" in data.decode():
-                #     print(f"A ping request message was received from Peer {sending_peer}")
-                #     response = "Ping_response"
-                #     LTPsock.sendto(response.encode("utf-8"),addr)
+                if "Ping_request" in pickle.loads(data):
+                    print(f"A ping request message was received from Peer {sending_peer}")
+                    response = pickle.dumps({"Ping_response":time.ctime()})
+                    LTPsock.sendto(response,addr)
                 # else:#download and reassemble file
-                print("downloading file")
-                packets = data.split()
-                if isdone:
-                    f_name = None
-                    next_seq = 0
-                    Seq = 0
-                    Ack = 0
-                    buffer = {}
-                    isdone =False
-                if not f_name:
-                    f_name = packets[2].decode()
-                    f = open("received_"+f_name+".pdf",'wb')
-                    next_seq += len(f_name)
-                if packets[0] == next_seq:
-                    content = packets[2] + '\r\n'
-                    f.write(content.encode('utf-8'))
-                    Ack = next_seq
-                    next_seq += len(packets[2])
-                    if buffer:
-                        while min(list(buffer.keys())) == next_seq:
-                            content = buffer[next_seq] + '\r\n'
-                            f.write(content.encode('utf-8'))
-                            del buffer[next_seq]
-                            Ack = next_seq
-                            next_seq += len(packets[2])
-                else:
-                    if(len(packets) == 2):
-                        isdone == True
-                    else:
-                        buffer[packets[0]]=packets[2]
-                acknowledgement = str(Seq) + " "+str(Ack) + '\r\n'
-                LTPsock.sendto(acknowledgement.encode("utf-8"),addr)
+            # print("downloading file")
+            
+            # # while True:
+            # if not data: break
+            # d += data
+                # chunk = pickle.loads(d)
+                # print(data)
+                # if isdone:
+                #     f_name = None
+                #     next_seq = 0
+                #     Seq = 0
+                #     Ack = 0
+                #     buffer = {}
+                #     isdone =False
+                # if not f_name:
+                #     f_name = chunk[2].decode()
+                #     f = open("received_"+f_name+".pdf",'wb')
+                #     next_seq += len(f_name)
+                # if chunk[0] == next_seq:
+                #     content = chunk[2] + '\r\n'
+                #     f.write(content.encode('utf-8'))
+                #     Ack = next_seq
+                #     next_seq += len(chunk[2])
+                #     if buffer:
+                #         while min(list(buffer.keys())) == next_seq:
+                #             content = buffer[next_seq] + '\r\n'
+                #             f.write(content.encode('utf-8'))
+                #             del buffer[next_seq]
+                #             Ack = next_seq
+                #             next_seq += len(chunk[2])
+                # else:
+                #     if(len(chunk) == 2):
+                #         isdone == True
+                #     else:
+                #         buffer[chunk[0]]=chunk[2]
+                # acknowledgement = str(Seq) + " "+str(Ack) + '\r\n'
+                # LTPsock.sendto(acknowledgement.encode("utf-8"),addr)
+        c = pickle.loads(d)
+        print(c)
         LTPsock.close()
 
 class ReqPing(threading.Thread):
@@ -96,10 +106,10 @@ class ReqPing(threading.Thread):
     def run(self):
         mysock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
         while True:
-            message = "Ping_request"+ str(time.ctime()) + "\r\n"
-            mysock.sendto(message.encode("utf-8"),(self.host,self.port))
-            data = mysock.recv(1024)
-            if data:
+            toSendPing = pickle.dumps({"Ping_request": time.ctime()})
+            mysock.sendto(toSendPing,(self.host,self.port))
+            recvResponse = pickle.loads(mysock.recv(1024))
+            if "Ping_response" in recvResponse:
                 print(f"A ping response message was received from Peer {self.port - 50000}")
         mysock.close()
 
@@ -155,25 +165,35 @@ class transferFile(threading.Thread):
         mysock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
         # send file
         f = open(self.file_name+".pdf",'rb')
-        data = f.read(1024)
+        data = f.read(MSS)
         seq = 0
         Ack = 0
         # send the file name first
-        toSendPacket = str(seq) + " "+str(Ack)+ " " + self.file_name + '\r\n'
-        mysock.sendto(toSendPacket.encode("utf-8"),(self.host,self.port))
+        toSendPacket = {}
+        toSendPacket["seq"] = seq
+        toSendPacket["ack"] = Ack
+        toSendPacket["data"] = self.file_name
+        #pickle data
+        pickle_out = pickle.dumps(toSendPacket)
+        mysock.sendto(pickle_out,(self.host,self.port))
         seq += len(self.file_name)
         while data:
-            toSendPacket = str(seq)+ " " +str(Ack) + " " 
-            mysock.sendto(toSendPacket.encode('utf-8')+data+'\r\n'.encode('utf-8'),(self.host,self.port))
+            toSendPacket["seq"] = seq
+            toSendPacket["ack"] = Ack
+            toSendPacket["data"] = data
+            pickle_out = pickle.dumps(toSendPacket)
+            mysock.sendto(pickle_out,(self.host,self.port))
             seq += len(data)
-            data = f.read(1024)
+            data = f.read(MSS)
         # send a data with 0 size to indicate the completion of transfering file
-        toSendPacket = str(seq)+ " "+str(Ack) + " " 
-        mysock.sendto(toSendPacket.encode('utf-8')+data+'\r\n'.encode('utf-8'),(self.host,self.port))
+        toSendPacket = {}
+        toSendPacket["seq"] = seq
+        toSendPacket["ack"] = Ack
+        toSendPacket["data"] = data
+        pickle_out = pickle.dumps(toSendPacket)
+        mysock.sendto(pickle_out,(self.host,self.port))
         self.file_name = None
         mysock.close()
-
-
 
 def Myhash(filename):
     return int(filename)%256
@@ -187,15 +207,15 @@ def main():
     # listening to ping request
     # # one thread for listen to ping request
     peer.Listening()
-    # # two threads for sending ping request to two successors
-    # # send ping request
-    # peer.Pinging()
+    # two threads for sending ping request to two successors
+    # send ping request
+    peer.Pinging()
     #step3: Requesting a file
-    while True:
-        command = input("enter a command: ").split()
-        if len(command) == 2:
-            t = transferFile(peer.host,peer.fir_successor+50000,"2012")
-            t.start()
+    # while True:
+    #     command = input("enter a command: ").split()
+    #     if len(command) == 2:
+    #         t = transferFile(peer.host,peer.fir_successor+50000,"2012")
+    #         t.start()
     
     #step4
 
